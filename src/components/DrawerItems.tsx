9cb60d65f5485c2a4841bd83e22eb8fc5eb4567c
Neeraj Malkani <neerajmalkani33@gmail.com>
2022-06-12 01:44:06 +0530

    Drawer selected page css added

--- src/components/DrawerItems.tsx
+++ src/components/DrawerItems.tsx
@@ -6,6 +6,7 @@ import LocalOffer from "@mui/icons-material/LocalOffer";
 import InsertComment from "@mui/icons-material/InsertComment";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useNavigate } from "react-router-dom";
+import { theme } from "../theme/AppTheme";
 
 interface OpenObj {
   open: [boolean, Dispatch<SetStateAction<boolean>>];
@@ -21,6 +22,7 @@ const DrawerItems = ({ open }: OpenObj) => {
       items: [
         {
           title: "Activity",
+          isActive: window.location.href.toLowerCase().includes("activity"),
           navigation: () => {
             open[1](false);
             setTimeout(() => {
@@ -28,10 +30,11 @@ const DrawerItems = ({ open }: OpenObj) => {
             }, 300);
           },
         },
-        { title: "Service" },
-        { title: "Unit of Sales" },
+        { title: "Service", isActive: window.location.href.toLowerCase().includes("service") },
+        { title: "Unit of Sales", isActive: window.location.href.toLowerCase().includes("unitofsales") },
         {
           title: "Category",
+          isActive: window.location.href.toLowerCase().includes("category"),
           navigation: () => {
             open[1](false);
             setTimeout(() => {
@@ -39,12 +42,12 @@ const DrawerItems = ({ open }: OpenObj) => {
             }, 300);
           },
         },
-        { title: "Products" },
-        { title: "Service Product" },
-        { title: "Department" },
-        { title: "Location Type" },
-        { title: "Designation" },
-        { title: "E-Way Bill" },
+        { title: "Products", isActive: window.location.href.toLowerCase().includes("products") },
+        { title: "Service Product", isActive: window.location.href.toLowerCase().includes("serviceproduct") },
+        { title: "Department", isActive: window.location.href.toLowerCase().includes("department") },
+        { title: "Location Type", isActive: window.location.href.toLowerCase().includes("locationtype") },
+        { title: "Designation", isActive: window.location.href.toLowerCase().includes("designation") },
+        { title: "E-Way Bill", isActive: window.location.href.toLowerCase().includes("ewaybill") },
       ],
     },
     { title: "Users", icon: <Group />, expanded: false },
@@ -59,13 +62,13 @@ const DrawerItems = ({ open }: OpenObj) => {
             <ListItemIcon sx={{ minWidth: 0, mr: 3, justifyContent: "center" }}>{item.icon}</ListItemIcon>
             <Typography>{item.title}</Typography>
           </AccordionSummary>
-          <AccordionDetails style={{ padding: 0, backgroundColor: "#ededed" }}>
+          <AccordionDetails style={{ padding: 0 }}>
             {item.items &&
               item.items.map((listItem, index) => {
                 return (
                   <Box key={index}>
-                    <ListItemButton sx={{ minHeight: 48, justifyContent: "initial", px: 2.5 }} onClick={listItem.navigation}>
-                      <ListItemText primary={listItem.title} sx={{ opacity: 1 }} />
+                    <ListItemButton selected={listItem.isActive} sx={{ minHeight: 48, justifyContent: "initial", px: 2.5, backgroundColor: listItem.isActive ? theme.palette.primary.main + " !important" : "#ededed" }} onClick={listItem.navigation}>
+                      <ListItemText primary={listItem.title} sx={{ opacity: 1, color: listItem.isActive ? theme.palette.primary.contrastText : theme.palette.text.primary }} />
                     </ListItemButton>
                     <Divider />
                   </Box>
--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -7,7 +7,7 @@ export const theme = createTheme({
       main: "#39979B",
     },
     secondary: {
-      main: "#FFFF8F",//"#f1a7fe",
+      main: "#FFFF8F", //"#f1a7fe",
     },
   },
 });
