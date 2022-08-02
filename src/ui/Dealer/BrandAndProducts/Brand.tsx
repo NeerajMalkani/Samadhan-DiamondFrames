15496ef8400f4318969973046daadbd99d473d70
Suraj Gupta <suraj.gupta169@gmail.com>
2022-08-02 22:46:04 +0530

    fixes

--- src/ui/Dealer/BrandAndProducts/Brand.tsx
+++ src/ui/Dealer/BrandAndProducts/Brand.tsx
@@ -334,13 +334,13 @@ const BrandPage = () => {
       .then((response: any) => {
         if (response.data && response.data.code === 200) {
           if (response.data.data) {
-            debugger;
-            response.data.data.map((el) => {
-              buyerCategoryFullData.find((a)=>{
-                return a.buyerCategoryID === el.id;
-              })
+            // debugger;
+            // response.data.data.map((el) => {
+            //   buyerCategoryFullData.find((a)=>{
+            //     return a.buyerCategoryID === el.id;
+            //   })
 
-            });
+            // });
             
             // const arrBuyerDiscountData = [];
             // const arrSelectedBuyerDiscountData = [];
@@ -552,7 +552,6 @@ const BrandPage = () => {
           }
         })
         .catch((e) => {
-          debugger;
           handleCancelClick();
           setSnackMsg(communication.NetworkError);
           setSnackbarType("error");
