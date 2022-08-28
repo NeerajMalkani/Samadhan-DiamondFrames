f8ec60375ebb04583dbc6d27ee824ccef2fe7418
Centeos <107580733+Centeos@users.noreply.github.com>
2022-08-28 16:09:19 +0530

    Update JSCommonFunction
    added blood group enum

--- src/utils/JSCommonFunction.js
+++ src/utils/JSCommonFunction.js
@@ -13,3 +13,14 @@ export const retrunValueFromLocation = (arr, key, isState = false) => {
 export const retrunSumID = (blankData) => {
   return blankData.map((bill) => bill.id).reduce((acc, amount) => acc.toString() + amount.toString());
 };
+
+const BloodGroup = {
+  "A+": 1,
+  "O+": 2,
+  "B+": 3,
+  "AB+":4,
+  "A-": 5,
+  "O-": 6,
+  "B-": 7,
+  "AB-":8
+};
